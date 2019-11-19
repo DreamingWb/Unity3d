3bab7f35c48c3f47253b3bcf460df08f3bbe3328
TempterCyn <48970853+TempterCyn@users.noreply.github.com>
2019-11-19 13:51:33 +0800

    hw9

---
+++ UISystem/Assets/IMGUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IMGUI : MonoBehaviour {
+	public float health;
+	private float res;
+	public Slider healthSlider;
+	void Start(){
+		health = 1.0f;
+		res = 1.0f;
+	}
+	void OnGUI(){
+		if (GUI.Button (new Rect(250, 20, 40, 20), "+")) {
+			if (res + 0.2f > 1.0f) res = 1.0f;
+			else res = res + 0.2f;
+		}
+			if (GUI.Button (new Rect(250, 50, 40, 20), "-")) {
+			if (res - 0.2f < 0.0f) res = 0.0f;
+			else res = res - 0.2f;
+		}
+		health = Mathf.Lerp(health, res, 0.05f);
+		GUI.color = Color.red;
+		GUI.HorizontalScrollbar(new Rect(20, 20, 200, 20), 0.0f, health, 0.0f, 1.0f);
+		healthSlider.value = health*100;
+	}
+}
